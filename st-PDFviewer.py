851de559561b3dcc80dc2fa8d332deb5ed36c955
Sam Brasil <76713595+Samuel-Brasil@users.noreply.github.com>
2024-04-08 16:27:17 -0300

    Update st-PDFviewer.py

--- st-PDFviewer.py
+++ st-PDFviewer.py
@@ -1,15 +1,3 @@
-import streamlit as st
-import base64
-
-def display_pdf(file):
-    base64_pdf = base64.b64encode(file.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" style="border: none;"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-uploaded_file = st.file_uploader("Select a PDF file", type="pdf")
-
-#if uploaded_file is not None:
-#    display_pdf(uploaded_file)
 
 from streamlit_pdf_reader import pdf_reader
 
