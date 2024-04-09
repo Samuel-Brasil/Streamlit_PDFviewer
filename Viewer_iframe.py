35f0b813e99fe461516ecabab803580fb23e8149
Sam Brasil <76713595+Samuel-Brasil@users.noreply.github.com>
2024-04-08 23:34:40 -0300

    Update Viewer_iframe.py

--- Viewer_iframe.py
+++ Viewer_iframe.py
@@ -11,6 +11,7 @@ if uploaded_file is not None:
     # Convert the uploaded PDF file to base64
     base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
     # Embed the PDF in an iframe
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+#    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000px" type="application/pdf"></iframe>'
     # Display the PDF
     st.markdown(pdf_display, unsafe_allow_html=True)
